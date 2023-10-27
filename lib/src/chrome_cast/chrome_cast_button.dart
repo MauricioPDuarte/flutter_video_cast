16c973e9995a947df7b246596ad346ac5ecb6791
Mauricio Pruss Duarte <103187097+MauricioPrussDuarte@users.noreply.github.com>
2023-10-27 09:50:30 -0300

    changes

--- lib/src/chrome_cast/chrome_cast_button.dart
+++ lib/src/chrome_cast/chrome_cast_button.dart
@@ -13,7 +13,7 @@ typedef void OnRequestFailed(String error);
 class ChromeCastButton extends StatelessWidget {
   /// Creates a widget displaying a ChromeCast button.
   ChromeCastButton(
-      {Key key,
+      {required Key key,
       this.size = 30.0,
       this.color = Colors.black,
       this.onButtonCreated,
@@ -21,9 +21,7 @@ class ChromeCastButton extends StatelessWidget {
       this.onSessionEnded,
       this.onRequestCompleted,
       this.onRequestFailed})
-      : assert(
-            defaultTargetPlatform == TargetPlatform.iOS ||
-                defaultTargetPlatform == TargetPlatform.android,
+      : assert(defaultTargetPlatform == TargetPlatform.iOS || defaultTargetPlatform == TargetPlatform.android,
             '$defaultTargetPlatform is not supported by this plugin'),
         super(key: key);
 
@@ -37,19 +35,19 @@ class ChromeCastButton extends StatelessWidget {
   /// Callback method for when the button is ready to be used.
   ///
   /// Used to receive a [ChromeCastController] for this [ChromeCastButton].
-  final OnButtonCreated onButtonCreated;
+  final OnButtonCreated? onButtonCreated;
 
   /// Called when a cast session has started.
-  final VoidCallback onSessionStarted;
+  final VoidCallback? onSessionStarted;
 
   /// Called when a cast session has ended.
-  final VoidCallback onSessionEnded;
+  final VoidCallback? onSessionEnded;
 
   /// Called when a cast request has successfully completed.
-  final VoidCallback onRequestCompleted;
+  final VoidCallback? onRequestCompleted;
 
   /// Called when a cast request has failed.
-  final OnRequestFailed onRequestFailed;
+  final OnRequestFailed? onRequestFailed;
 
   @override
   Widget build(BuildContext context) {
@@ -69,27 +67,19 @@ class ChromeCastButton extends StatelessWidget {
   Future<void> _onPlatformViewCreated(int id) async {
     final ChromeCastController controller = await ChromeCastController.init(id);
     if (onButtonCreated != null) {
-      onButtonCreated(controller);
+      onButtonCreated!(controller);
     }
     if (onSessionStarted != null) {
-      _chromeCastPlatform
-          .onSessionStarted(id: id)
-          .listen((_) => onSessionStarted());
+      _chromeCastPlatform.onSessionStarted(id: id).listen((_) => onSessionStarted!());
     }
     if (onSessionEnded != null) {
-      _chromeCastPlatform
-          .onSessionEnded(id: id)
-          .listen((_) => onSessionEnded());
+      _chromeCastPlatform.onSessionEnded(id: id).listen((_) => onSessionEnded!());
     }
     if (onRequestCompleted != null) {
-      _chromeCastPlatform
-          .onRequestCompleted(id: id)
-          .listen((_) => onRequestCompleted());
+      _chromeCastPlatform.onRequestCompleted(id: id).listen((_) => onRequestCompleted!());
     }
     if (onRequestFailed != null) {
-      _chromeCastPlatform
-          .onRequestFailed(id: id)
-          .listen((event) => onRequestFailed(event.error));
+      _chromeCastPlatform.onRequestFailed(id: id).listen((event) => onRequestFailed!(event.error));
     }
   }
 }
